5368ea3eda0e67f84ce9afcf537b3557545a93c7
sebastianpadilla02 <98917806+sebastianpadilla02@users.noreply.github.com>
2022-05-11 10:51:34 -0500

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -3,38 +3,96 @@ from tkinter.font import BOLD, Font
 from tkinter import filedialog
 from tkinter import ttk
 
-ventana = Tk()
-ventana.geometry("500x500")
-ventana.title("From Data to Species")
-ventana.configure(bg='green')
+ventana_eleccion = Tk()
+ventana_eleccion.geometry("300x300")
+ventana_eleccion.title("From Data to Species")
+ventana_eleccion.configure(bg='green')
+
+def nueva_ventana(num_fotos):
+  ventana = Toplevel(ventana_eleccion)
+  ventana.geometry("500x500")
+  ventana.title("From Data to Species")
+  ventana.configure(bg='green')
+
+  titulolbl = Label(ventana, text = "From Data to Species",font = fuentetitulo )
+  titulolbl.pack()
+
+  instruc = Label(ventana, text = "Ingrese una foto para analizar el tipo de planta de la foto", font = fuente)
+  instruc.pack()
+
+  def leer_link():
+    ola = link.get()
+    print(ola)
+
+  # Crear caja de texto.
+  link = ttk.Entry(ventana, width=50)
+  # Posicionarla en la ventana.
+  link.place(x=50, y=100)
+
+  botonlink = Button(ventana, text ="Ingresar link", command= leer_link)
+  botonlink.place(x=80, y=125)
+
+  botonarchivo = Button(ventana, text="Abrir Archivo", command = abrir_archivo)
+  botonarchivo.pack()
+
+  ventana.mainloop
+  #ventana_eleccion.destroy()
+
 
 def abrir_archivo():
   archivo = filedialog.askopenfilename(title="Abrir Archivo", initialdir = "C:/", filetypes = (("Archivos jpg", "*.jpg"), ("Archivos png", "*.png"), ("Archivos jpeg", "*.jpeg")))
   print(archivo)
 
-def leer_link():
-  ola = link.get()
-  print(ola)
+
+
+def num1():
+  num_fotos = 1
+  nueva_ventana(num_fotos)
+  ventana_eleccion.withdraw()
+
+def num2():
+  num_fotos = 2
+  nueva_ventana(num_fotos)
+  ventana_eleccion.destroy()
+
+def num3():
+  num_fotos = 3
+  nueva_ventana(num_fotos)
+  ventana_eleccion.destroy()
+
+def num4():
+  num_fotos = 4
+  nueva_ventana(num_fotos)
+  ventana_eleccion.destroy()
+
+def num5():
+  num_fotos = 5
+  nueva_ventana(num_fotos)
+  ventana_eleccion.destroy()
 
 fuente = Font(family="Roboto Cn", size=14)
 
 fuentetitulo = Font(family="Roboto Cn", size=18, weight= "bold")
 
-titulolbl = Label(ventana, text = "From Data to Species",font = fuentetitulo )
+titulolbl = Label(ventana_eleccion, text = "From Data to Species",font = fuentetitulo )
 titulolbl.pack()
 
-instruc = Label(ventana, text = "Ingrese una foto para analizar el tipo de planta de la foto", font = fuente)
+instruc = Label(ventana_eleccion, text = "Cantidad de fotos", font = fuente)
 instruc.pack()
 
-# Crear caja de texto.
-link = ttk.Entry(width=50)
-# Posicionarla en la ventana.
-link.place(x=50, y=100)
+boton_1=Button(text="1", command = num1, width=10)
+boton_1.place(x=110, y=70)
 
-botonlink = Button(ventana, text ="Ingresar link", command= leer_link)
-botonlink.place(x=80, y=125)
+boton_2=Button(text="2", command = num1, width=10)
+boton_2.place(x=110, y=110)
 
-botonarchivo = Button(ventana, text="Abrir Archivo", command = abrir_archivo)
-botonarchivo.pack()
+boton_3=Button(text="3", command = num1, width=10)
+boton_3.place(x=110,y=150)
 
-ventana.mainloop()
+boton_4=Button(text="4", command = num1, width=10)
+boton_4.place(x=110, y=190)
+
+boton_5=Button(text="5", command = num1, width=10)
+boton_5.place(x=110, y=230)
+
+ventana_eleccion.mainloop()
