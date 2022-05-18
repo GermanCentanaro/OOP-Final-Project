16c762ca06056c8adff735893ea220a3b61be6d0
sebastianpadilla02 <98917806+sebastianpadilla02@users.noreply.github.com>
2022-05-18 16:26:45 -0500

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -1,7 +1,10 @@
+from operator import length_hint
 from tkinter import *
 from tkinter.font import BOLD, Font
 from tkinter import filedialog
 from tkinter import ttk
+from turtle import color
+from PIL import Image
 
 ventana_eleccion = Tk()
 ventana_eleccion.geometry("300x300")
@@ -25,20 +28,28 @@ def nueva_ventana(num_fotos):
   instruc = Label(ventana, text = "Ingrese una foto para analizar el tipo de planta de la foto", font = fuente, bg='green')
   instruc.pack()
 
+  link_entrada = Entry(ventana, width=70)
+  link_entrada.pack()
+
   def leer_link():
     link= link_entrada.get()
+    link_entrada.delete(0,END)
     print(link)
 
-  # Crear caja de texto.
-  link_entrada = ttk.Entry(ventana, width=50)
-  # Posicionarla en la ventana.
-  link_entrada.place(x=50, y=100)
-
   botonlink = Button(ventana, text ="Ingresar link", command= leer_link)
-  botonlink.place(x=80, y=125)
+  botonlink.place(x=200, y=90)
+
+  #otraopcion = Label(ventana, text = "Otra opción", font = fuente, bg='green')
+  #otraopcion.pack()
+
+  parte_label = Label(ventana, text = "Escriba la parte de la plante en específico de la que va a ingresar foto", font = fuente, bg = 'green')
+  parte_label.place(x=60, y=310)
+
+  parte_planta = Entry(ventana, width=40)
+  parte_planta.place(x=60, y=300)
 
   botonarchivo = Button(ventana, text="Abrir Archivo", command = abrir_archivo)
-  botonarchivo.place(x=220, y=300)
+  botonarchivo.place(x=210, y=170)
 
   cerrar = Button(ventana, text="Cerrar", command=closewindow)
   cerrar.place(x=220,y=450)
@@ -51,7 +62,8 @@ def closewindow():
 
 def abrir_archivo():
   archivo = filedialog.askopenfilename(title="Abrir Archivo", initialdir = "C:/", filetypes = (("Archivos jpg", "*.jpg"), ("Archivos png", "*.png"), ("Archivos jpeg", "*.jpeg")))
-  print(archivo)
+  im = Image.open(archivo)
+  im.show()
 
 def num1():
   num_fotos = 1
@@ -61,22 +73,22 @@ def num1():
 def num2():
   num_fotos = 2
   nueva_ventana(num_fotos)
-  ventana_eleccion.destroy()
+  ventana_eleccion.withdraw()
 
 def num3():
   num_fotos = 3
   nueva_ventana(num_fotos)
-  ventana_eleccion.destroy()
+  ventana_eleccion.withdraw()
 
 def num4():
   num_fotos = 4
   nueva_ventana(num_fotos)
-  ventana_eleccion.destroy()
+  ventana_eleccion.withdraw()
 
 def num5():
   num_fotos = 5
   nueva_ventana(num_fotos)
-  ventana_eleccion.destroy()
+  ventana_eleccion.withdraw()
 
 fuente = Font(family="Roboto Cn", size=14)
 
