a54426e6b2193d158b3cbeefab2a375e55bafb23
sebastianpadilla02 <98917806+sebastianpadilla02@users.noreply.github.com>
2022-05-30 22:25:06 -0500

    Update analisis.py

--- analisis.py
+++ analisis.py
@@ -3,7 +3,7 @@ import requests
 import json
 from pprint import pprint
 from listas import *
-from german import *
+from interfaz import VentanaEjecucion
 
 class Proceso:
   def hacer(num_fotos):
@@ -11,6 +11,7 @@ class Proceso:
     api_endpoint = f"https://my-api.plantnet.org/v2/identify/all?api-key={API_KEY}"
     
     if(num_fotos == 1):
+      ruta1 = VentanaEjecucion.reg.FotoI
       ruta1 = listaarchivo[0]
       image_path_1 = ruta1
       image_data_1 = open(image_path_1, 'rb')
@@ -149,4 +150,68 @@ class Proceso:
     print(listanombreplanta)
     print(listanombrecientifico)
     print(listapuntaje)
-    #pprint(species)
+
+class Registro():
+	def __init__(self):
+		self.__resultado = None
+		self.__fotoI = None
+		self.__fotoU = None
+
+	@property
+	def resultado(self):
+		return self.__resultado
+		
+	@resultado.setter
+	def resultado(self, texto):
+		self.__resultado = texto
+
+	def añadir(self, foto):
+		if (self.__fotoI == None):
+			self.__fotoI = foto
+			self.__fotoU = foto
+		else:
+			self.__fotoU.next = foto
+			self.__fotoU = foto
+
+	def mostrarFotos(self):
+		foto = self.__fotoI
+		while (foto != None):
+			foto.mostrar()
+			foto = foto.next
+
+	def __str__(self):
+		return "RESULTADO: \n" + self.__resultado
+
+class Foto():
+	def __init__(self, tlink):
+		self.__ruta = tlink
+		self.__next = None
+		self.__data = open(tlink, 'rb')
+	
+	@property
+	def ruta(self):
+		return self.__ruta
+		
+	@ruta.setter
+	def ruta(self, texto):
+		self.__ruta = texto
+		
+	@property
+	def next(self):
+		return self.__next
+    
+	@next.setter
+	def next(self, nueva_foto):
+		self.__next = nueva_foto
+	
+	@property
+	def data(self):
+		return self.__data
+
+	def mostrar(self):
+		im = Image.open(self.__ruta)
+		im.show()
+	
+	def __str__(self):
+		return "Link: " + str(self.__ruta) + " - Data: " + str(self.__data)
+
