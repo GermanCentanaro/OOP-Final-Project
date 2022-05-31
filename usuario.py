8b34ef96b405f08b60c3d2e0946f92ba3d4906ab
sebastianpadilla02 <98917806+sebastianpadilla02@users.noreply.github.com>
2022-05-31 10:00:50 -0500

    Create usuario.py

---
+++ usuario.py
@@ -0,0 +1,24 @@
+class Usuario:
+    def __init__(self, name, nombre, contraseña):
+        self.name = name
+        self.nombre = nombre
+        self.contraseña = contraseña
+
+    def Iniciar(self, nombre, contraseña):
+        archivo = open("usuarios.txt", "r")
+        linea = archivo.readline()
+        salir = False
+        while(salir == False) and linea != "":
+            vector = linea.split("\t")
+            if(vector[0] == nombre and vector[1] == contraseña):
+                salir = True
+            linea = archivo.readline()
+        archivo.close()
+        return salir
+
+    def Registrar(self, nombre, contraseña):
+        archivo = open("usuarios.txt", "a")
+
+        archivo.write(nombre + "\t" + contraseña)
+        print("\nUsuario ingresado con exito.\n")
+        archivo.close
